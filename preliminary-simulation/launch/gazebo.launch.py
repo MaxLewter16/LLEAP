e526e14aac4e62d22285dce6e7d469244172d043
Kyle Quinlan <kylequinlan09@gmail.com>
2023-02-16 19:22:08 -0800

    Use the standard ros_gazebo and switch back urdf
    Switch back URDF to what it was so it works with RVIZ and gazebo. Also
    use ros_gazebo instead of ign_gazebo since it might work better with
    moveit and you can use GPU acceleration on WSL.

--- preliminary-simulation/launch/gazebo.launch.py
+++ preliminary-simulation/launch/gazebo.launch.py
@@ -28,10 +28,10 @@ def generate_launch_description():
 
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('ros_ign_gazebo'), 'launch'), '/ign_gazebo.launch.py']),
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
         )
  
-    spawn_entity = Node(package='ros_ign_gazebo', executable='create',
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                     arguments=['-topic', 'robot_description',
                                 '-entity', 'my_bot'],
                     output='screen')
